04e989e6e4eca1cd8af9bff3ab55d5522633e666
Gorish09 <mahajangorish9@gmail.com>
2024-06-12 14:37:01 +0530

    name change

---
+++ src/interfac.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public class interfac {
+    public static void main(String[]args) {
+        ArrayList<String> city = new ArrayList<String>();
+        city.add("Gurdaspur");
+        city.add("Dinanagar");
+        city.add("Dorangla");
+        city.add("Delhi");
+        System.out.println(city);
+
+        ArrayList<String> name = new ArrayList<String>();
+        name.add("gorish");
+        name.add("rahul");
+        name.add("raman");
+        System.out.println(name);
+
+    }
+}
