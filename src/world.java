2bcef9710d3fa206e4d2a87c24b9c2e2cf34b7cb
Gorish09 <mahajangorish9@gmail.com>
2024-06-12 11:20:02 +0530

    ASDFSADF

--- src/world.java
+++ src/world.java
@@ -1,6 +1,6 @@
 public class world {
     static void myStaticMethod(){
-        System.out.println("hello my name is Gorish");
+        System.out.println("hello my name is Vivek ");
     }
     static void myPublicMethod(){
         System.out.println("i am 19 years old");
@@ -9,6 +9,6 @@ public class world {
     public static void main(String[] args) {
         myStaticMethod();
         world myObj = new world();
-        myObj.myPublicMethod();
+         myObj.myPublicMethod();
     }
 }
