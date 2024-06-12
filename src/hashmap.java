f93af7fa8d91e062c20b298240309d3623b20f28
Gorish09 <mahajangorish9@gmail.com>
2024-06-12 19:54:05 +0530

    name changef

---
+++ src/hashmap.java
@@ -0,0 +1,30 @@
+public class hashmap {
+    public static void main(String[] args) {
+        int[] numbers = new int[5];
+        String[] names = new String[5];
+        float[] marks = new float[5];
+
+        Student[] students = new Student[5];
+        //Student gorish;
+        Student gorish = new Student();
+        gorish.rno =2;
+        gorish.name="Gorish Mahajan";
+        gorish.marks = 92;
+        System.out.println(gorish.rno);
+        System.out.println(gorish.name);
+        System.out.println(gorish.marks);
+
+    }
+
+
+    }
+
+        class Student {
+            int rno;
+            String name;
+            float marks;
+        }
+
+
+
+
