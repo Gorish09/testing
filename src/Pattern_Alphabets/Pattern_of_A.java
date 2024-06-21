053108adba245ea1127394b8a18bb755041fb3a3
Gorish09 <mahajangorish9@gmail.com>
2024-06-21 16:24:31 +0530

    name changef

--- src/Pattern_Alphabets/Pattern_of_A.java
+++ src/Pattern_Alphabets/Pattern_of_A.java
@@ -1,2 +1,24 @@
 package Pattern_Alphabets;public class Pattern_of_A {
+    public static void main(String[] args)
+    {
+        int i, j, n=8;
+
+        for (i = 0; i<=n; i++)
+        {
+
+            for (j = 0; j<= n / 1; j++)
+            {
+
+                if ((j == 0 || j == n / 1) && i != 0 ||
+
+                        i == 0  && j != n / 1 ||
+
+                        i == n / 2)
+                    System.out.print("#");
+                else
+                    System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
 }
